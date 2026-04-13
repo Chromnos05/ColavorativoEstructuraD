55ba1f22ff28263a185948fc36d3aabc8a159821
Oscar Mercado <omercador@unicartagena.edu.co>
2026-04-12 22:15:51 -0500

    matrices: punto 2 - recorrido por filas (tabla) y por columnas en Java

--- Oscar/matrices/Matriz.java
+++ Oscar/matrices/Matriz.java
@@ -9,5 +9,24 @@ public class Matriz {
         };
 
         System.out.println("Punto 1: Matriz 3x3 declarada e inicializada.");
+
+        // Actividad 2: Recorrido
+        // Imprimir la matriz en forma de tabla
+        System.out.println("\nImpresión de la matriz en forma de tabla:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Recorrer la matriz por columnas
+        System.out.println("\nRecorrido por columnas:");
+        for (int j = 0; j < matriz[0].length; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(" (Columna " + j + ")");
+        }
     }
 }
