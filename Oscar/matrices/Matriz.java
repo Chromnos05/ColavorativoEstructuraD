adbbdf49cb5483ab9a32cffa3d6b4b668f5a936c
Oscar Mercado <omercador@unicartagena.edu.co>
2026-04-12 22:15:39 -0500

    matrices: punto 1 - declaracion e inicializacion de matriz 3x3 en Java

---
+++ Oscar/matrices/Matriz.java
@@ -0,0 +1,13 @@
+public class Matriz {
+    public static void main(String[] args) {
+        // Actividad 1: Declaración e inicialización
+        // Crear una matriz 3x3 de enteros
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Punto 1: Matriz 3x3 declarada e inicializada.");
+    }
+}
